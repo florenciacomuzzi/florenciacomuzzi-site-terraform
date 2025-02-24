4c45ebfb3a161149ba36e9bf92fb56c9444127c1
Florencia Comuzzi <florencia.comuzzi@alticeusa.com>
2025-02-24 13:26:37 -0500

    add base

---
+++ providers.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    linode = {
+      source  = "linode/linode"
+      # version = "..."
+    }
+  }
+}
+
+# Configure the Linode Provider
+provider "linode" {
+  # token = "..."
+}
+
+resource "linode_instance" "web" {
+  label           = "simple_instance"
+  image           = "linode/ubuntu22.04"
+  region          = "us-central"
+  type            = "g6-standard-1"
+  authorized_keys = ["ssh-rsa AAAA...Gw== user@example.local"]
+  root_pass       = "this-is-not-a-safe-password"
+
+  tags       = ["foo"]
+  swap_size  = 256
+  private_ip = true
+}
