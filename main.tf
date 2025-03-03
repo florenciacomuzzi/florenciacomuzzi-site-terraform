0087f114ae8800e7a6f1e1f07665d5884a0097cf
Florencia Comuzzi <florencia.comuzzi@alticeusa.com>
2025-03-03 14:19:44 -0500

    dev env

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "linode_lke_cluster" "development" {
     label       = "development"
     k8s_version = "1.32"
     region      = "us-southeast"
-    tags        = ["prod"]
+    tags        = ["dev"]
 
     pool {
         type  = "g6-standard-1"
