6aa4765588a5c0998686fa21164304c460461627
Florencia <florenciabsalinas@gmail.com>
2025-03-02 19:19:08 -0500

    Create cluster (#4)
    * break up docs
    
    * break up docs (#3)
    
    * create cluster
    
    ---------
    
    Co-authored-by: Florencia Comuzzi <florencia.comuzzi@alticeusa.com>

--- main.tf
+++ main.tf
@@ -1,12 +1,11 @@
-resource "linode_instance" "web" {
-  label           = "simple_instance"
-  image           = "linode/ubuntu22.04"
-  region          = "us-central"
-  type            = "g6-standard-1"
-  authorized_keys = ["ssh-rsa AAAA...Gw== user@example.local"]
-  root_pass       = "this-is-not-a-safe-password"
+resource "linode_lke_cluster" "development" {
+    label       = "development"
+    k8s_version = "1.32"
+    region      = "us-southeast"
+    tags        = ["prod"]
 
-  tags       = ["foo"]
-  swap_size  = 256
-  private_ip = true
+    pool {
+        type  = "g6-standard-1"
+        count = 3
+    }
 }
