da87d97366d5c85f953a14496c11133bfff0ba9c
Flo Comuzzi <florenciabsalinas@gmail.com>
2025-03-05 19:59:13 -0500

    Revert "Create cluster (#4)" (#5)
    This reverts commit 6aa4765588a5c0998686fa21164304c460461627.

--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
-resource "linode_lke_cluster" "development" {
-    label       = "development"
-    k8s_version = "1.32"
-    region      = "us-southeast"
-    tags        = ["prod"]
+resource "linode_instance" "web" {
+  label           = "simple_instance"
+  image           = "linode/ubuntu22.04"
+  region          = "us-central"
+  type            = "g6-standard-1"
+  authorized_keys = ["ssh-rsa AAAA...Gw== user@example.local"]
+  root_pass       = "this-is-not-a-safe-password"
 
-    pool {
-        type  = "g6-standard-1"
-        count = 3
-    }
+  tags       = ["foo"]
+  swap_size  = 256
+  private_ip = true
 }
